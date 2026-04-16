6c5fb12fbf434621b3b8cffaf3cf80994040e007
tamurayuuiti <314takamorisan@gmail.com>
2026-04-16 17:55:43 +0900

    chore: プロジェクト初期環境を構築

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className="flex min-h-screen items-center justify-center bg-gray-100 text-gray-800">
+      <h1 className="text-2xl font-bold">New Project Start</h1>
+    </div>
+  )
+}
+
+export default App
